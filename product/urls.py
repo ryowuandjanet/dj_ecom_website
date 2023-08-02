71fb24708e53bcd3a25899001195959eafb2be3c
ryowuandjanet <ryowu0329@gmail.com>
2023-08-02 23:38:00 +0800

    Part 7

--- product/urls.py
+++ product/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from .views import (
     Home,
     ProductDetails,
-    CategoryDetails
+    CategoryDetails,
+    ProductList
 )
 
 urlpatterns = [
     path('',Home.as_view(), name='home'),
     path('product-details/<str:slug>/',ProductDetails.as_view(), name='product-details'),
     path('category-details/<str:slug>/',CategoryDetails.as_view(), name='category-details'),
+    path('product-list/',ProductList.as_view(), name='product-list'),
 ]
